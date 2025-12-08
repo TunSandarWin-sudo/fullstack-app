f63ba9feff32ab2b51bf669577295686beec8d44
TunSandarWin-sudo <tun.s67@rsu.ac.th>
2025-12-08 18:33:37 +0700

    k

--- 02_frontend/src/app.js
+++ 02_frontend/src/app.js
@@ -9,4 +9,15 @@ export default function App() {
     React.createElement(ItemList, null)
   );
 }
+import React from "react";
+import ItemList from "./components/ItemList.js";
+
+export default function App() {
+  return React.createElement(
+    "main",
+    { style: { padding: 24 } },
+    React.createElement("h1", null, "Menu"),
+    React.createElement(ItemList, null)
+  );
+}
 export { default } from "./App.js";
